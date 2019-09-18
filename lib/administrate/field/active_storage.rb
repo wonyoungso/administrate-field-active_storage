fb4f41a0ade07c46e3f16926d2ba67ab9857e75b
Wonyoung So <wso@mit.edu>
2019-09-17 21:55:29 -0400

    update

--- lib/administrate/field/active_storage.rb
+++ lib/administrate/field/active_storage.rb
@@ -65,8 +65,16 @@ module Administrate
         Rails.application.routes.url_helpers.send(destroy_path_helper, {:record_id => record_id, :attachment_id => attachment_id})
       end
 
-      delegate :attached?, to: :data 
-      delegate :attachments, to: :data# if !data.nil?
+      def attached?
+        self.photo.attached? rescue nil
+      end
+
+      # def attachments
+        # self.
+      # end
+
+      # delegate :attached?, to: :data 
+      # delegate :attachments, to: :data# if !data.nil?
     end
   end
 end
